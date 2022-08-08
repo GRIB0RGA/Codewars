3d38e1ee60e26901bae87a8dbec29f3e757ad4aa
Giorgi Iremadze <iremadzegiorgi01@gmail.com>
2022-08-08 14:43:26 +0400

    020-The-Feast-of-Many-Beasts.js

---
+++ Finished/8 kyu/020-The-Feast-of-Many-Beasts.js
@@ -0,0 +1,1 @@
+const feast = (beast, dish) => beast[0] === dish[0] && beast.slice(-1) === dish.slice(-1)
--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-
+const labelMaker = (name, num) => `0${num}-${name?.replaceAll(" ", "-")}.js`;
+console.log(labelMaker("The Feast of Many Beasts", 20));
