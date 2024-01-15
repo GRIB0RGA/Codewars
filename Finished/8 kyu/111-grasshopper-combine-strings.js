6012003c5160e51825486751dcb52956f736753a
Griborga <giorgi4@discovery.co.za>
2024-01-15 10:11:59 +0400

    [Kata-8] - 15/01/2024

---
+++ Finished/8 kyu/111-grasshopper-combine-strings.js
@@ -0,0 +1,1 @@
+const combineNames = (fn, ln) => `${fn} ${ln}`;
---
+++ Finished/8 kyu/113-this-is-a-problem.js
@@ -0,0 +1,7 @@
+function NameMe(first, last) {
+  return {
+    name: first + " " + last,
+    firstName: first,
+    lastName: last,
+  };
+}
